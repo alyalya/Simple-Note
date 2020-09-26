1988ae26047b1dec255842b9fa5f650778f6ccb9
Aliya Sadrtdinova <dorywhatever@gmail.com>
2020-09-26 18:00:21 +0300

    Initial Commit

---
+++ Swift Note/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift Note
+//
+//  Created by Aliia Vaganova on 9/26/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
