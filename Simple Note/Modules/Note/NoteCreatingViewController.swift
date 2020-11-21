83780e3a988f29946831ec91c2ba2101180cd7df
alyalya <dorywhatever@gmail.com>
2020-11-22 00:12:43 +0300

    fixed conflict

--- Simple Note/Modules/Note/NoteCreatingViewController.swift
+++ Simple Note/Modules/Note/NoteCreatingViewController.swift
@@ -73,7 +73,6 @@ private extension NoteCreatingViewController {
   @objc func handleButtonPress() {
     guard let titleFieldText = titleField.text,
           let textViewText = textView.text else { return }
-    dataService.data.append(NoteData(title: titleFieldText, text: textViewText))
     showAlert()
     clearScreen()
     let id = dataService.data.count + 1
@@ -100,24 +99,6 @@ private extension NoteCreatingViewController {
     let alert = UIAlertController(title: "🥳", message: "Твоя заметка сохранена", preferredStyle: .alert)
     alert.addAction(UIAlertAction(title: "🤝", style: .default, handler: nil))
     present(alert, animated: true, completion: nil)
-    let id = dataService.data.count + 1
-    if let initialText = initialText {
-      let index = dataService.data.firstIndex(where: {
-        guard let item = $0 else { return false }
-        return item.text == initialText
-      })
-      guard let unwrappedIndex = index else { return }
-      dataService.data[unwrappedIndex]?.title = titleFieldText
-      dataService.data[unwrappedIndex]?.text = textViewText
-    } else {
-      dataService.data.append(
-        NoteData(
-          id: id,
-          title: titleFieldText,
-          text: textViewText
-        )
-      )
-    }
   }
 
   func clearScreen() {
